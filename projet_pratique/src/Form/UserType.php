8f4ed597cff1b553ff81c3dda30c7d456e998aab
Antoine-PRN <titounet.perrin@gmail.com>
2024-06-14 13:51:38 +0200

    Modification d'IU et d'UX

--- projet_pratique/src/Form/UserType.php
+++ projet_pratique/src/Form/UserType.php
@@ -9,8 +9,13 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class UserType extends AbstractType
 {
@@ -37,12 +42,50 @@ class UserType extends AbstractType
       ])
       ->add('password', PasswordType::class, [
         'label' => 'Mot de passe',
-        'required' => true,
+        'required' => false,
+        'mapped' => false,
+      ])
+      ->add('newPassword', PasswordType::class, [
+        'label' => 'Choisissez un nouveau mot de passe',
+        'required' => false,
+        'mapped' => false,
+        'constraints' => [
+          new Regex([
+            'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/',
+            'message' => 'Votre mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spéciaux.',
+          ]),
+        ],
+      ])
+      ->add('newPasswordConfirm', PasswordType::class, [
+        'label' => 'Confirmez votre nouveau mot de passe',
+        'required' => false,
+        'mapped' => false,
       ])
       ->add('submit', SubmitType::class, [
         'label' => 'Mettre à jour',
         'attr' => ['class' => 'btn primary-button'],
       ]);
+
+    $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+      $form = $event->getForm();
+      $data = $form->getData();
+
+      $password = $form->get('password')->getData();
+      $newPassword = $form->get('newPassword')->getData();
+      $newPasswordConfirm = $form->get('newPasswordConfirm')->getData();
+
+      if (!empty($password)) {
+        if (empty($newPassword)) {
+          $form->get('newPassword')->addError(new FormError('Ce champ est obligatoire.'));
+        }
+        if (empty($newPasswordConfirm)) {
+          $form->get('newPasswordConfirm')->addError(new FormError('Ce champ est obligatoire.'));
+        }
+        if ($newPassword !== $newPasswordConfirm) {
+          $form->get('newPasswordConfirm')->addError(new FormError('Les mots de passe ne correspondent pas.'));
+        }
+      }
+    });
   }
 
   public function configureOptions(OptionsResolver $resolver)
